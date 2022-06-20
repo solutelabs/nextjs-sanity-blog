42497110a56f8daada0a74cad0f7b702a8ccb3ce
Rachit Thakker <rachit.t@solutelabs.com>
2022-06-21 00:40:10 +0530

    Configured for 'next export'

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'custom',
     domains: ['cdn.sanity.io'],
   },
+  basePath: isProd ? '/out' : '',
+  assetPrefix: isProd ? '/out/' : '',
 };
 
 module.exports = nextConfig;
